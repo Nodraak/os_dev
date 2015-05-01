9664a1371b861c6e5c2512eccaf62eaf9c3e7862
Adrien Chardon <adrienchardon@mailoo.org>
2015-05-01 02:09:04 +0200

    multiboot.h clean

--- inc/io/buffer.h
+++ inc/io/buffer.h
@@ -15,6 +15,6 @@ typedef struct _s_buffer
 void buffer_init(s_buffer *b);
 void buffer_push_char(s_buffer *b, char c);
 char buffer_pop_char(s_buffer *b);
-char *buffer_pop_str(s_buffer *b);
+void buffer_pop_str(s_buffer *b, char *buffer);
 
 #endif
